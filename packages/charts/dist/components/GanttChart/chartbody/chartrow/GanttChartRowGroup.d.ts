64aeb0c7b8d3ec3c6104628be5e7e1bc2d918447
Marcin Duda <marcin.duda@sap.com>
2024-08-30 11:05:22 +0200

    feat: add newest build

--- packages/charts/dist/components/GanttChart/chartbody/chartrow/GanttChartRowGroup.d.ts
+++ packages/charts/dist/components/GanttChart/chartbody/chartrow/GanttChartRowGroup.d.ts
@@ -1,16 +1,17 @@
 import React from 'react';
-import type { IGanttChartRow } from '../../types/GanttChartTypes.js';
-interface GanttChartRowGroupProps {
-    dataset: IGanttChartRow[];
-    rowHeight: number;
-    totalDuration: number;
-    GanttStart: number;
-    showTooltip: (...x: unknown[]) => void;
-    handleTaskClick: (task: Record<string, any>, event: React.MouseEvent) => void;
-    hideTooltip: () => void;
+import type { IGanttChartRow, OpenRowIndex, OpenSubRowIndexes } from '../../types/GanttChartTypes.js';
+export interface GanttChartRowGroupProps {
+  dataset: IGanttChartRow[];
+  rowHeight: number;
+  totalDuration: number;
+  GanttStart: number;
+  showTooltip: (...x: unknown[]) => void;
+  handleTaskClick: (task: Record<string, any>, event: React.MouseEvent) => void;
+  hideTooltip: () => void;
+  openRowIndex: OpenRowIndex;
+  openSubRowIndexes: OpenSubRowIndexes;
 }
 export declare const GanttChartRowGroup: {
-    ({ dataset, rowHeight, totalDuration, GanttStart, showTooltip, hideTooltip, handleTaskClick }: GanttChartRowGroupProps): React.JSX.Element;
-    displayName: string;
+  (props: GanttChartRowGroupProps): React.JSX.Element;
+  displayName: string;
 };
-export {};
