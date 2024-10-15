2f7e3957e061225a96f015256a8f47e4fd0c6a06
I560940 <bartosz.lipinski@sap.com>
2024-10-15 11:44:02 +0200

    chore: added latest build

--- packages/charts/dist/components/GanttChart/headers/GanttChartRowLabels.js
+++ packages/charts/dist/components/GanttChart/headers/GanttChartRowLabels.js
@@ -6,6 +6,7 @@ import { RowLabelItem } from './RowLabelItem.js';
 export const GanttChartRowLabels = (props) => {
     const { width, height, rowHeight, dataset, dataType, handleClick, handleSubClick, openRowIndex, openSubRowIndexes, numOfRows } = props;
     const classes = useStyles();
+    const statusPadding = '0px';
     const style = {
         width: width,
         height: `${numOfRows * rowHeight + ROW_CONTRACT_DURATION_HEIGHT}px`
@@ -15,13 +16,16 @@ export const GanttChartRowLabels = (props) => {
             React.createElement("div", { style: { height: ROW_CONTRACT_DURATION_HEIGHT } }),
             dataset.map((row, rowIndex) => {
                 const showCollapseIcon = row.subRows?.length > 0 && dataType === 'label';
-                return (React.createElement(React.Fragment, { key: `row-${rowIndex}` },
-                    React.createElement(RowLabelItem, { key: `item-${rowIndex}`, padding: showCollapseIcon ? '-10px' : '10px', collapseIcon: showCollapseIcon ? (openRowIndex === rowIndex ? (React.createElement(Icon, { name: "navigation-down-arrow" })) : (React.createElement(Icon, { name: "navigation-right-arrow" }))) : null, onClick: () => handleClick(rowIndex), isActive: true, rowHeight: rowHeight, style: { display: dataType === 'status' ? 'flex' : 'initial', border: "solid" } }, row[dataType]),
+                return (React.createElement("div", { key: `row-${rowIndex}`, style: { transform: dataType === 'label' && 'translateX(-20px)' } },
+                    React.createElement(RowLabelItem, { key: `item-${rowIndex}`, padding: showCollapseIcon ? '0px' : dataType === 'status' ? statusPadding : '25px', collapseIcon: showCollapseIcon ? (openRowIndex === rowIndex ? (React.createElement(Icon, { name: "navigation-down-arrow" })) : (React.createElement(Icon, { name: "navigation-right-arrow" }))) : null, onClick: () => handleClick(rowIndex), isActive: true, rowHeight: rowHeight, style: {
+                            display: dataType === 'status' ? 'flex' : 'initial',
+                            border: 'solid'
+                        } }, row[dataType]),
                     row.subRows?.map((subRow, detailIndex) => {
                         const showCollapseIcon = subRow.subRows?.length > 0 && dataType === 'label';
                         return (React.createElement(React.Fragment, { key: `row-detail-${detailIndex}` },
-                            React.createElement(RowLabelItem, { key: `detail-${detailIndex}`, padding: dataType === 'label' ? '20px' : '10px', collapseIcon: showCollapseIcon ? (openSubRowIndexes[`${rowIndex}-${detailIndex}`] ? (React.createElement(Icon, { name: "navigation-down-arrow" })) : (React.createElement(Icon, { name: "navigation-right-arrow" }))) : null, onClick: () => handleSubClick(rowIndex, detailIndex), isActive: openRowIndex === rowIndex, rowHeight: rowHeight }, subRow[dataType]),
-                            subRow.subRows?.map((subDetail, subDetailIndex) => (React.createElement(RowLabelItem, { key: `subdetail-${subDetailIndex}`, padding: dataType === 'label' ? '40px' : '10px', isActive: openSubRowIndexes[`${rowIndex}-${detailIndex}`], rowHeight: rowHeight }, subDetail[dataType])))));
+                            React.createElement(RowLabelItem, { key: `detail-${detailIndex}`, padding: showCollapseIcon ? '10px' : dataType === 'status' ? statusPadding : '35px', collapseIcon: showCollapseIcon ? (openSubRowIndexes[`${rowIndex}-${detailIndex}`] ? (React.createElement(Icon, { name: "navigation-down-arrow" })) : (React.createElement(Icon, { name: "navigation-right-arrow" }))) : null, onClick: () => handleSubClick(rowIndex, detailIndex), isActive: openRowIndex === rowIndex, rowHeight: rowHeight, style: { display: dataType === 'status' ? 'flex' : 'initial' } }, subRow[dataType]),
+                            subRow.subRows?.map((subDetail, subDetailIndex) => (React.createElement(RowLabelItem, { key: `subdetail-${subDetailIndex}`, padding: dataType === 'label' ? '50px' : statusPadding, isActive: openSubRowIndexes[`${rowIndex}-${detailIndex}`], rowHeight: rowHeight }, subDetail[dataType])))));
                     })));
             }))));
 };
