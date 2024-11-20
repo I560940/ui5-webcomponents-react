68605bb20ce6fffe4ad165cf43c5a75bcac5613e
I560940 <bartosz.lipinski@sap.com>
2024-11-20 12:59:01 +0100

    feat(GanttChart): added tooltip for cut text

--- packages/charts/dist/components/GanttChart/headers/GanttChartRowLabels.js
+++ packages/charts/dist/components/GanttChart/headers/GanttChartRowLabels.js
@@ -20,12 +20,15 @@ export const GanttChartRowLabels = (props) => {
                     React.createElement(RowLabelItem, { key: `item-${rowIndex}`, padding: showCollapseIcon ? '0px' : dataType === 'status' ? statusPadding : '25px', collapseIcon: showCollapseIcon ? (openRowIndexes.includes(rowIndex) ? (React.createElement(Icon, { name: "navigation-down-arrow" })) : (React.createElement(Icon, { name: "navigation-right-arrow" }))) : null, onClick: () => handleClick(rowIndex), isActive: true, rowHeight: rowHeight, style: {
                             display: dataType === 'status' ? 'flex' : 'initial',
                             border: 'solid'
-                        } }, row[dataType]),
+                        } },
+                        React.createElement("span", { title: row[dataType] }, row[dataType])),
                     row.subRows?.map((subRow, detailIndex) => {
                         const showCollapseIcon = subRow.subRows?.length > 0 && dataType === 'label';
                         return (React.createElement(React.Fragment, { key: `row-detail-${detailIndex}` },
-                            React.createElement(RowLabelItem, { key: `detail-${detailIndex}`, padding: showCollapseIcon ? '10px' : dataType === 'status' ? statusPadding : '35px', collapseIcon: showCollapseIcon ? (openSubRowIndexes[`${rowIndex}-${detailIndex}`] ? (React.createElement(Icon, { name: "navigation-down-arrow" })) : (React.createElement(Icon, { name: "navigation-right-arrow" }))) : null, onClick: () => handleSubClick(rowIndex, detailIndex), isActive: openRowIndexes.includes(rowIndex), rowHeight: rowHeight, style: { display: dataType === 'status' ? 'flex' : 'initial' } }, subRow[dataType]),
-                            subRow.subRows?.map((subDetail, subDetailIndex) => (React.createElement(RowLabelItem, { key: `subdetail-${subDetailIndex}`, padding: dataType === 'label' ? '50px' : statusPadding, isActive: openSubRowIndexes[`${rowIndex}-${detailIndex}`], rowHeight: rowHeight }, subDetail[dataType])))));
+                            React.createElement(RowLabelItem, { key: `detail-${detailIndex}`, padding: showCollapseIcon ? '10px' : dataType === 'status' ? statusPadding : '35px', collapseIcon: showCollapseIcon ? (openSubRowIndexes[`${rowIndex}-${detailIndex}`] ? (React.createElement(Icon, { name: "navigation-down-arrow" })) : (React.createElement(Icon, { name: "navigation-right-arrow" }))) : null, onClick: () => handleSubClick(rowIndex, detailIndex), isActive: openRowIndexes.includes(rowIndex), rowHeight: rowHeight, style: { display: dataType === 'status' ? 'flex' : 'initial' } },
+                                React.createElement("span", { title: subRow[dataType] }, subRow[dataType])),
+                            subRow.subRows?.map((subDetail, subDetailIndex) => (React.createElement(RowLabelItem, { key: `subdetail-${subDetailIndex}`, padding: dataType === 'label' ? '50px' : statusPadding, isActive: openSubRowIndexes[`${rowIndex}-${detailIndex}`], rowHeight: rowHeight },
+                                React.createElement("span", { title: subDetail[dataType] }, subDetail[dataType]))))));
                     })));
             }))));
 };
